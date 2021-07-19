1b4a6a9dd094f7bae5ad14afb2d353a32f04cee9
tribe-05-jadu <samitkarim9@gmail.com>
2021-07-19 17:35:05 +0400

    user profile

--- src/components/mainSettingsProfile/MainSettingsProfile.js
+++ src/components/mainSettingsProfile/MainSettingsProfile.js
@@ -1,10 +1,69 @@
 import React from 'react'
 import './MainSettingsProfile.css'
+import LeftIcon from '../../assets/svg/left-green-arrow.svg'
+import AccountBoxOutlinedIcon from '@material-ui/icons/AccountBoxOutlined';
+import HorizontalLine from '../../assets/svg/Horizontal-spliter-1.svg'
+import { Avatar } from '@material-ui/core';
 
 const MainSettingsProfile = () => {
     return (
-        <div>
-            
+        <div className='mainSettingsProfile'>
+            <div className='mainSettingsProfile__heading'>
+                <img src={LeftIcon} />
+                <h4>Profile</h4>
+            </div>
+            <div className='mainSettingsProfile__picture'>
+                <Avatar className='mainSettingsProfile__avatar' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Full Name</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>User-ID/Nickname</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Number</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Email</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Adress</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Delete Account</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
+            <div className='mainSettingsProfile__name'>
+                <AccountBoxOutlinedIcon />
+                <h4>Logout</h4>
+            </div>
+            <div className='mainSettings__divider'>
+                <img src={HorizontalLine} alt='line' />
+            </div>
         </div>
     )
 }
