88847a30267b9693a6c78f1cda2f2cc6e562f25b
Zaid Sultan <zaidsultan09@gmail.com>
2021-08-04 05:06:10 +0400

    fixed responsiveness

--- src/components/mainFeedFilter5/MainFeedFilter5.js
+++ src/components/mainFeedFilter5/MainFeedFilter5.js
@@ -33,7 +33,7 @@ const MainFeedFilter5 = () => {
             <NavLink exact to='/chef/home/feed' activeClassName='activefilter52' className='mainFeedFilter5__feed'>Feeds</NavLink>
             <NavLink to='/chef/home/feed/recipe' activeClassName='activefilter52' className='mainFeedFilter5__recipe' >Recipes</NavLink>
             <NavLink to='/chef/home/feed/food' activeClassName='activefilter52' className='mainFeedFilter5__food'>Food</NavLink>
-            <NavLink to='/chef/home/feed/service' activeClassName='activefilter52' className='mainFeedFilter5__service'>Serivce</NavLink>
+            <NavLink to='/chef/home/feed/service' activeClassName='activefilter52' className='mainFeedFilter5__service'>Service</NavLink>
             <NavLink to='/chef/home/feed/masterclass' activeClassName='activefilter52' className='mainFeedFilter5__masterclass'>e-Masterclass</NavLink>
 
         </div>
