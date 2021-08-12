8139f824552661841fcdbd8d98c8c904916eafcd
Sameer Katija <sameerkatija@gmail.com>
2021-08-13 04:30:52 +0500

    Added sign in and signup and error

---
+++ src/Auth/verification.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+// const AuthSignUp = async ({ email, password, userType }) => {
+//   const user = await axios.post("/api/user/register", {
+//     email,
+//     password,
+//     userType,
+//   });
+//   if (user.status === 200) return { data: user.data, status: 200 };
+//   // let error:
+//   return { err: "Something went wrong", status: 400 };
+// };
+// export default AuthSignUp;
