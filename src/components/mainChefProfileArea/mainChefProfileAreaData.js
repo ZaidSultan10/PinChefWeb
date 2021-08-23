21ff1c195f3061aeee28a3b29af28bd82989b0ff
Zaid Sultan <zaidsultan09@gmail.com>
2021-08-24 00:10:19 +0400

    chef profile input complete

---
+++ src/components/mainChefProfileArea/mainChefProfileAreaData.js
@@ -0,0 +1,487 @@
+const dateOfBirth = [ 
+    {
+    days : '1'
+},
+{
+    days : '2'
+},{
+    days : '3'
+},{
+    days : '4'
+},{
+    days : '5'
+},
+{
+    days : '6'
+},
+{
+    days : '7'
+},{
+    days : '8'
+},{
+    days : '9'
+},{
+    days : '10'
+},{
+    days : '11'
+},
+{
+    days : '12'
+},{
+    days : '13'
+},{
+    days : '14'
+},{
+    days : '15'
+},
+{
+    days : '16'
+},
+{
+    days : '17'
+},{
+    days : '18'
+},{
+    days : '19'
+},{
+    days : '20'
+},{
+    days : '21'
+},
+{
+    days : '22'
+},{
+    days : '23'
+},{
+    days : '24'
+},{
+    days : '25'
+},
+{
+    days : '26'
+},
+{
+    days : '27'
+},{
+    days : '28'
+},{
+    days : '29'
+},{
+    days : '30'
+},{
+    days : '31'
+}]
+
+const monthOfBirth = [
+    {
+        month:'January'
+    },{
+        month:'Feburary'
+    },{
+        month:'March'
+    },{
+        month:'April'
+    },{
+        month:'May'
+    },{
+        month:'June'
+    },{
+        month:'July'
+    },{
+        month:'August'
+    },{
+        month:'September'
+    },{
+        month:'October'
+    },{
+        month:'November'
+    },{
+        month:'December'
+    }
+]
+
+const cuisineList = [
+    {
+        cuisine:'Afghani'
+    },
+    {
+        cuisine:'African'
+    },
+    {
+        cuisine:'Albanian'
+    },
+    {
+        cuisine:'American'
+    },
+    {
+        cuisine:'Apulian'
+    },
+    {
+        cuisine:'Arabic'
+    },
+    {
+        cuisine:'Argentinean'
+    },
+    {
+        cuisine:'Armenian'
+    },
+    {
+        cuisine:'Asian'
+    },
+    {
+        cuisine:'Assyrian'
+    },
+    {
+        cuisine:'Australian'
+    },
+    {
+        cuisine:'Austrian'
+    },
+    {
+        cuisine:'Bahamian'
+    },
+    {
+        cuisine:'Bangladeshi'
+    },
+    {
+        cuisine:'Basque'
+    },
+    {
+        cuisine:'Beijing cuisine'
+    },
+    {
+        cuisine:'Belgian'
+    },
+    {
+        cuisine:'Brazilian'
+    },
+    {
+        cuisine:'British'
+    },
+    {
+        cuisine:'Bulgarian'
+    },
+    {
+        cuisine:'Burmese'
+    },
+    {
+        cuisine:'Cajun & Creole'
+    },
+    {
+        cuisine:'Cambodian'
+    },
+    {
+        cuisine:'Campania'
+    },
+    {
+        cuisine:'Canadian'
+    },
+    {
+        cuisine:'Cantonese'
+    },
+    {
+        cuisine:'Caribbean'
+    },
+    {
+        cuisine:'Catalan'
+    },
+    {
+        cuisine:'Central American'
+    },
+    {
+        cuisine:'Central Asian'
+    },
+    {
+        cuisine:'Central European'
+    },
+    {
+        cuisine:'Central-Italian'
+    },
+    {
+        cuisine:'Chilean'
+    },
+    {
+        cuisine:'Chinese'
+    },
+    {
+        cuisine:'Colombian'
+    },
+    {
+        cuisine:'Contemporary'
+    },
+    {
+        cuisine:'Croatian'
+    },
+    {
+        cuisine:'Cuban'
+    },
+    {
+        cuisine:'Czech'
+    },
+    {
+        cuisine:'Danish'
+    },
+    {
+        cuisine:'Deli'
+    },
+    {
+        cuisine:'Eastern European'
+    },
+    {
+        cuisine:'Ecuadorean'
+    },
+    {
+        cuisine:'Egyptian'
+    },
+    {
+        cuisine:'Emilian'
+    },
+    {
+        cuisine:'Ethiopian'
+    },
+    {
+        cuisine:'European'
+    },
+    {
+        cuisine:'Filipino'
+    },
+    {
+        cuisine:'French'
+    },
+    {
+        cuisine:'Fusion'
+    },
+    {
+        cuisine:'Georgian'
+    },
+    {
+        cuisine:'German'
+    },
+    {
+        cuisine:'Greek'
+    },
+    {
+        cuisine:'Hawaiian'
+    },
+    {
+        cuisine:'Healthy'
+    },
+    {
+        cuisine:'Hong Kong'
+    },
+    {
+        cuisine:'Hungarian'
+    },
+    {
+        cuisine:'Indian'
+    },
+    {
+        cuisine:'Indonesian'
+    },
+    {
+        cuisine:'International'
+    },
+    {
+        cuisine:'Irish'
+    },
+    {
+        cuisine:'Israeli'
+    },
+    {
+        cuisine:'Italian'
+    },
+    {
+        cuisine:'Jamaican'
+    },
+    {
+        cuisine:'Japanese'
+    },
+    {
+        cuisine:'Japanese Fusion'
+    },
+    {
+        cuisine:'Kaiseki'
+    },
+    {
+        cuisine:'Korean'
+    },
+    {
+        cuisine:'Latin'
+    },
+    {
+        cuisine:'Lazio'
+    },
+    {
+        cuisine:'Lebanese'
+    },
+    {
+        cuisine:'Malaysian'
+    },
+    {
+        cuisine:'Mediterranean'
+    },
+    {
+        cuisine:'Mexican'
+    },
+    {
+        cuisine:'Middle Eastern'
+    },
+    {
+        cuisine:'Moroccan'
+    },
+    {
+        cuisine:'Native American'
+    },
+    {
+        cuisine:'Neapolitan'
+    },
+    {
+        cuisine:'Nepali'
+    },
+    {
+        cuisine:'New Zealand'
+    },
+    {
+        cuisine:'Nigerian'
+    },
+    {
+        cuisine:'Nonya'
+    },
+    {
+        cuisine:'Northern-Italian'
+    },
+    {
+        cuisine:'NorthWestern Chinese'
+    },
+    {
+        cuisine:'Norwegian'
+    },
+    {
+        cuisine:'Pakistani'
+    },
+    {
+        cuisine:'Persian'
+    },
+    {
+        cuisine:'Peruvian'
+    },
+    {
+        cuisine:'Pizza'
+    },
+    {
+        cuisine:'Polish'
+    },
+    {
+        cuisine:'Polynesian'
+    },
+    {
+        cuisine:'Portuguese'
+    },
+    {
+        cuisine:'Puerto Rican'
+    },
+    {
+        cuisine:'Romagna'
+    },
+    {
+        cuisine:'Romana'
+    },
+    {
+        cuisine:'Romanian'
+    },
+    {
+        cuisine:'Russian'
+    },
+    {
+        cuisine:'Salvadoran'
+    },
+    {
+        cuisine:'Sardinian'
+    },
+    {
+        cuisine:'Scandinavian'
+    },
+    {
+        cuisine:'Scottish'
+    },
+    {
+        cuisine:'Shanghai'
+    },
+    {
+        cuisine:'Sicilian'
+    },
+    {
+        cuisine:'Singaporean'
+    },
+    {
+        cuisine:'South American'
+    },
+    {
+        cuisine:'Southern-Italian'
+    },
+    {
+        cuisine:'Southwestern'
+    },
+    {
+        cuisine:'Spanish'
+    },
+    {
+        cuisine:'Sri Lankan'
+    },
+    {
+        cuisine:'Sushi'
+    },
+    {
+        cuisine:'Swedish'
+    },
+    {
+        cuisine:'Swiss'
+    },
+    {
+        cuisine:'Szechuan'
+    },
+    {
+        cuisine:'Taiwanese'
+    },
+    {
+        cuisine:'Thai'
+    },
+    {
+        cuisine:'Tibetan'
+    },
+    {
+        cuisine:'Tunisian'
+    },
+    {
+        cuisine:'Turkish'
+    },
+    {
+        cuisine:'Turkmen'
+    },
+    {
+        cuisine:'Tuscan'
+    },
+    {
+        cuisine:'Ukrainian'
+    },
+    {
+        cuisine:'Uzbek'
+    },
+    {
+        cuisine:'Venezuelan'
+    },
+    {
+        cuisine:'Vietnamese'
+    },
+    {
+        cuisine:'Xinjiang'
+    },
+    {
+        cuisine:'Yunnan'
+    },
+
+]
+
+export {dateOfBirth}
+export {monthOfBirth}
+export {cuisineList}
