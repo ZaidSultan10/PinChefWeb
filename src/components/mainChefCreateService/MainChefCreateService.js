f79ee3d8e6c33e82dd040eabcd2491a56ddb6a6c
Zaid Sultan <zaidsultan09@gmail.com>
2021-08-27 00:16:33 +0400

    service form done

--- src/components/mainChefCreateService/MainChefCreateService.js
+++ src/components/mainChefCreateService/MainChefCreateService.js
@@ -13,8 +13,43 @@ const MainChefCreateService = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalIsOpen1, setModalIsOpen1] = useState(false);
 
-  //   const [imageUpload, setImageUpload] = useState("");
-  //   const [serviceType, setServiceType] = useState("");
+    const [imageUpload, setImageUpload] = useState("");
+    const [serviceType, setServiceType] = useState("");
+    const [description,setDescription] = useState('')
+    const [serviceDays, setServiceDays] = useState('')
+    const [price,setPrice] = useState('')
+
+    const handleChange = (event) => {
+        switch (event.target.name) {
+          case "description":
+            setDescription(event.target.value);
+            break;
+          case "serviceType":
+            setServiceType(event.target.value);
+            break;
+          case "imageUpload":
+            setImageUpload(event.target.value);
+            break;
+          case "serviceDays":
+            setServiceDays(event.target.value);
+            break;
+            case "price":
+            setPrice(event.target.value);
+            break;
+          default:
+            break;
+        }
+      };
+      const handleSubmit = (event) => {
+        const obj = {
+          description,
+          serviceType,
+          imageUpload,
+          serviceDays,
+          price
+        };
+        console.log(obj);
+      };
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -62,7 +97,7 @@ const MainChefCreateService = () => {
           </div>
         </div>
       </Modal>
-      <Modal
+      {/*<Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         className="myservicetypemodal1"
@@ -76,7 +111,7 @@ const MainChefCreateService = () => {
           <h3>Go to customers address to cook</h3>
           <h3>Cook Online with Chef</h3>
         </div>
-      </Modal>
+      </Modal> */}
       <div className="mainChefCreateService">
         <div className="mainChefCreateService__container">
           <div className="mainChefCreateService__heading">
@@ -84,40 +119,47 @@ const MainChefCreateService = () => {
             <CloseIcon onClick={openModal1} className="post-close-icon" />
           </div>
           <div className="mainChefCreateService__addImage">
-            <input multiple required name="imageUploader" />
+            <input multiple required type='file' name="imageUpload" value={imageUpload} onChange={handleChange} />
           </div>
-          <div onClick={openModal} className="mainChefCreateService__cuisine">
+          <div /*onClick={openModal}*/ className="mainChefCreateService__cuisine">
             <h5>Service Type</h5>
-            <select disabled id="service__type__option">
-              <option>Choose Service Type</option>
+            <select required name='serviceType' value={serviceType} onChange={handleChange} id="service__type__option">
+                <option>Cook and Deliver</option>
+                <option>Cook and Ship</option>
+                <option>Cook for Takeout/Away</option>
+                <option>Host guests and cook</option>
+                <option>Go to customers address to cook</option>
+                <option>Cook Online with Chef</option>
             </select>
           </div>
 
           <div className="mainChefCreateService__ingredients">
             <h5>Description about Serivce</h5>
-            <textarea rows="7" placeholder="Write all the ingredients" />
+            <textarea required name='description' value={description} onChange={handleChange} rows="7" placeholder="Write all the ingredients" />
           </div>
           <div className="mainChefCreateService__days">
             <h5>
               Service Days and Hours<span>*</span>
             </h5>
-            <select id="service__type__option">
+            <select required name='serviceDays' value={serviceDays} onChange={handleChange} id="service__type__option">
               <option>Monday</option>
               <option>tuesday</option>
               <option>Wednesday</option>
               <option>Thursday</option>
               <option>Friday</option>
+              <option>Saturday</option>
+              <option>Sunday</option>
             </select>
           </div>
           <div className="mainChefCreateService__name">
             <h5>Price</h5>
-            <input placeholder="Price.." />
+            <input required name='price' value={price} onChange={handleChange} placeholder="Price.." />
           </div>
           <div className="mainChefCreateService__buttons">
             <Button onClick={openModal1} className="service-cancel-button">
               Cancel
             </Button>
-            <Button className="service-post-button">Post</Button>
+            <Button onClick={handleSubmit} className="service-post-button">Post</Button>
           </div>
         </div>
       </div>
