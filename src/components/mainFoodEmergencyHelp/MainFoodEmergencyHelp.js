e6887834abfcd0ba8ed86ec3e6115d01fa11b20e
Zaid Sultan <zaidsultan09@gmail.com>
2021-08-27 01:01:57 +0400

    food emergency help form

--- src/components/mainFoodEmergencyHelp/MainFoodEmergencyHelp.js
+++ src/components/mainFoodEmergencyHelp/MainFoodEmergencyHelp.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LeftIcon from '../../assets/svg/left-green-arrow.svg'
 import './MainFoodEmergencyHelp.css'
 import InfoIcon from '../../assets/svg/info-icon-red.svg'
 import Ads from '../ads/Ads'
 import { Button } from '@material-ui/core'
+import { cuisineList, dietsList } from '../mainChefCreateRecipe/mainChefCreateRecipeData'
 
 
 
 const MainFoodEmergencyHelp = () => {
+
+    const [cuisines,setCuisines] = useState('')
+    const [dietType, setDietType] = useState('')
+    const [description, setDescription] = useState('')
+
+    const handleChange = (event) => {
+        switch (event.target.name) {
+          case "description":
+            setDescription(event.target.value);
+            break;
+          case "cuisines":
+            setCuisines(event.target.value);
+            break;
+          case "dietType":
+            setDietType(event.target.value);
+            break;
+          default:
+            break;
+        }
+      };
+      const handleSubmit = (event) => {
+        const obj = {
+          description,
+          cuisines,
+          dietType,
+        };
+        console.log(obj);
+      };
+
+
     return (
         <div className='mainFoodEmergencyHelp'>
             <div className='mainFoodEmergencyHelp__header'>
@@ -25,30 +56,30 @@ your request and the ones available will contact you.
             </div>
             <div className='mainFoodEmergencyHelp__cuisine'>
                 <h5>Cuisine Specialities<span>*</span></h5>
-                <select id='mainFoodEmergencyHelp__cuisineOptions'>
-                    <option>Monday</option>
-                    <option>tuesday</option>
-                    <option>Wednesday</option>
-                    <option>Thursday</option>
-                    <option>Friday</option>
+                <select required name='cuisines' value={cuisines} onChange={handleChange} id='mainFoodEmergencyHelp__cuisineOptions'>
+                    {
+                        cuisineList.map((cuisine) => (
+                            <option>{cuisine.cuisine}</option>
+                        ))
+                    }
                 </select>
             </div>
             <div className='mainFoodEmergencyHelp__dietary'>
                 <h5>Dietary<span>*</span></h5>
-                <select id='mainFoodEmergencyHelp__dietOptions'>
-                    <option>Monday</option>
-                    <option>tuesday</option>
-                    <option>Wednesday</option>
-                    <option>Thursday</option>
-                    <option>Friday</option>
+                <select required name='dietType' value={dietType} onChange={handleChange} id='mainFoodEmergencyHelp__dietOptions'>
+                    {
+                        dietsList.map((diet) => (
+                            <option>{diet.diet}</option>
+                        ))
+                    }
                 </select>
             </div>
             <div className='mainFoodEmergencyHelp__desc'>
                 <h5>Description<span>*</span></h5>
-                <textarea rows='7' />
+                <textarea required name='description' value={description} onChange={handleChange} rows='7' />
             </div>
             <div className='mainFoodEmergencyHelp__button'>
-                <Button>Send</Button>
+                <Button onClick={handleSubmit}>Send</Button>
             </div>
             <div className='mainFoodEmergencyHelp__ads'>
                 <Ads />
